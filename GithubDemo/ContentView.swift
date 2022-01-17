7d465ea9873a6dff359f9837af821f748c628a7a
RobSmy <rmsmythe@hotmail.com>
2022-01-17 15:51:58 -0500

    Initial Commit

---
+++ GithubDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GithubDemo
+//
+//  Created by Rob Smythe on 2022-01-17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GithubDemo/GithubDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubDemoApp.swift
+//  GithubDemo
+//
+//  Created by Rob Smythe on 2022-01-17.
+//
+
+import SwiftUI
+
+@main
+struct GithubDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
